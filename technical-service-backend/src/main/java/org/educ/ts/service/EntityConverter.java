68223549ce77c03d9227db969b9de78931c25e1e
aphzl <54284276+aphzl@users.noreply.github.com>
2021-10-17 03:44:44 +0500

    fix mapping date and creator

--- technical-service-backend/src/main/java/org/educ/ts/service/EntityConverter.java
+++ technical-service-backend/src/main/java/org/educ/ts/service/EntityConverter.java
@@ -6,16 +6,26 @@ import org.educ.ts.model.dto.UserDto;
 import org.educ.ts.model.entity.Device;
 import org.educ.ts.model.entity.Request;
 import org.educ.ts.model.entity.User;
+import org.educ.ts.repository.DeviceRepository;
+import org.educ.ts.repository.RequestRepository;
 import org.springframework.stereotype.Component;
 
-import java.sql.Timestamp;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static java.util.Optional.ofNullable;
 
 @Component
 public class EntityConverter {
+
+    private final DeviceRepository deviceRepository;
+    private final RequestRepository requestRepository;
+
+    public EntityConverter(DeviceRepository deviceRepository, RequestRepository requestRepository) {
+        this.deviceRepository = deviceRepository;
+        this.requestRepository = requestRepository;
+    }
 
     public Device toEntity(DeviceDto dto) {
         if (dto == null) {
@@ -55,17 +65,20 @@ public class EntityConverter {
             return null;
         }
 
+        Device device = deviceRepository.findBySerialNumber(dto.getDeviceSerialNumber());
+        Optional<Request> existingRequest = requestRepository.findById(dto.getId());
+
         return Request.builder()
                 .id(dto.getId())
                 .status(dto.getStatus())
                 .contactInfo(dto.getContactInfo())
                 .problemDescription(dto.getProblemDescription())
                 .resolveDescription(dto.getResolveDescription())
-                .createdAt(ofNullable(dto.getCreatedAt()).map(Timestamp::new).orElse(null))
-                .updatedAt(ofNullable(dto.getUpdatedAt()).map(Timestamp::new).orElse(null))
-                .createdBy(dto.getCreatedBy())
-                .updatedBy(dto.getUpdatedBy())
-                .device(toEntity(dto.getDevice()))
+                .createdAt(existingRequest.map(Request::getCreatedAt).orElse(null))
+                .updatedAt(existingRequest.map(Request::getUpdatedAt).orElse(null))
+                .createdBy(existingRequest.map(Request::getCreatedBy).orElse(null))
+                .updatedBy(existingRequest.map(Request::getUpdatedBy).orElse(null))
+                .device(device)
                 .assignedTo(dto.getAssignedTo())
                 .build();
     }
