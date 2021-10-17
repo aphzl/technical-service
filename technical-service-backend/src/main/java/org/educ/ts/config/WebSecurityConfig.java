faab789a69ad13978b33a0059e6dea927743402b
aphzl <54284276+aphzl@users.noreply.github.com>
2021-10-17 19:30:56 +0500

    add /static to security ignore

--- technical-service-backend/src/main/java/org/educ/ts/config/WebSecurityConfig.java
+++ technical-service-backend/src/main/java/org/educ/ts/config/WebSecurityConfig.java
@@ -21,7 +21,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
                 .antMatchers("/api/login")
-                .antMatchers("/");
+                .antMatchers("/")
+                .antMatchers("/static/**");
     }
 
     @Override
